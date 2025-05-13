4fe1a68fe954fdd0281e8347d54ae409cd1b2b1e
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2025-05-13 13:43:02 +0600

    first commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="my-10 text-lg font-bold text-amber-400">
+        Welcome to Real State
+      </Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./app/**/*.{ks,jsx,ts,tsx}",
+    "./components/**/*.{ks,jsx,ts,tsx}",
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

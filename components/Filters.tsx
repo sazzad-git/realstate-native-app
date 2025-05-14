2d53a50b64aa9f1a23cccaffbe1a12f9290e3655
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2025-05-15 02:55:15 +0600

    complete home and profile page

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,11 +1,53 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { categories } from "@/constants/data";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 
 const Filters = () => {
+  const params = useLocalSearchParams<{ filter?: string }>();
+
+  const [selectedCategory, setSelectedCategory] = useState(
+    params.filter || "All"
+  );
+
+  const handleCategoryPress = (category: string) => {
+    if (selectedCategory === category) {
+      setSelectedCategory("All");
+      router.setParams({ filter: "All" });
+      return;
+    }
+    setSelectedCategory(category);
+    router.setParams({ filter: category });
+  };
+
   return (
-    <View>
-      <Text>Filters</Text>
-    </View>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="mt-3 mb-2"
+    >
+      {categories.map((item, index) => (
+        <TouchableOpacity
+          onPress={() => handleCategoryPress(item.category)}
+          key={index}
+          className={`flex flex-col items-start px-4 py-2 mr-4 rounded-full ${
+            selectedCategory === item.category
+              ? "bg-primary-300"
+              : "bg-primary-100 border border-primary-200"
+          }`}
+        >
+          <Text
+            className={`text-sm ${
+              selectedCategory === item.category
+                ? "text-white font-rubik-bold mt-0.5"
+                : "text-black-300 font-rubik"
+            }`}
+          >
+            {item.title}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
   );
 };
 

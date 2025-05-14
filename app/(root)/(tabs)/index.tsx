78bdb4c1dbe3a27855d0a2117a5cb7c0562c8018
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2025-05-15 02:17:22 +0600

    Add Product cards

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,34 +1,56 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="h-full bg-white">
-      <View className="px-5">
-        <View className="flex flex-row justify-between items-center mt-5">
-          <View className="flex flex-row items-center">
-            <Image source={images.avatar} className="rounded-full size-12" />
-            <View className="flex flex-col justify-center items-start ml-2">
-              <Text className="text-xs font-rubik text-black-100">
-                Good Morning
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32"
+      >
+        <View className="px-5">
+          <View className="flex flex-row justify-between items-center mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="rounded-full size-12" />
+              <View className="flex flex-col justify-center items-start ml-2">
+                <Text className="text-xs font-rubik text-black-100">
+                  Good Morning
+                </Text>
+                <Text className="text-base font-rubik-medium text-black-300">
+                  Sazzadur
+                </Text>
+              </View>
+            </View>
+            <Image source={icons.bell} className="size-6" />
+          </View>
+
+          <Search />
+
+          <View className="my-5">
+            <View className="flex flex-row justify-between items-center">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Featured
               </Text>
-              <Text className="text-base font-rubik-medium text-black-300">
-                Sazzadur
-              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View className="flex flex-row gap-5 mt-5">
+              <FeaturedCard />
+              <FeaturedCard />
             </View>
           </View>
-          <Image source={icons.bell} className="size-6" />
-        </View>
-
-        <Search />
-
-        <View className="my-5">
           <View className="flex flex-row justify-between items-center">
             <Text className="text-xl font-rubik-bold text-black-300">
-              Featured
+              Our Recommendation
             </Text>
             <TouchableOpacity>
               <Text className="text-base font-rubik-bold text-primary-300">
@@ -36,8 +58,15 @@ export default function Index() {
               </Text>
             </TouchableOpacity>
           </View>
+
+          <Filters />
+
+          <View className="flex flex-row gap-5 mt-5">
+            <Card />
+            <Card />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,12 +1,88 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const Cards = () => {
+interface Props {
+  onPress?: () => void;
+}
+
+export const FeaturedCard = ({ onPress }: Props) => {
   return (
-    <View>
-      <Text>Cards</Text>
-    </View>
+    <TouchableOpacity
+      onPress={onPress}
+      className="flex relative flex-col items-start w-60 h-80"
+    >
+      <Image source={images.japan} className="rounded-2xl size-full" />
+      <Image
+        source={images.cardGradient}
+        className="absolute bottom-0 rounded-2xl size-full"
+      />
+      <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
+        <Image source={icons.star} className="size-3.5" />
+        <Text className="ml-1 text-xs font-rubik-bold text-primary-300">
+          4.4
+        </Text>
+      </View>
+
+      <View className="flex absolute inset-x-5 bottom-5 flex-col items-start">
+        <Text
+          className="text-xl text-white font-rubik-extrabold"
+          numberOfLines={1}
+        >
+          Modern Apartment
+        </Text>
+        <Text className="text-base text-white font-rubik">
+          22 W 15th St, New York
+        </Text>
+
+        <View className="flex flex-row justify-between items-center w-full">
+          <Text className="text-xl text-white font-rubik-extrabold">
+            $2,500
+          </Text>
+
+          <Image source={icons.heart} className="size-5" />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
-export default Cards;
+export const Card = ({ onPress }: Props) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="relative flex-1 px-3 py-4 mt-4 w-full bg-white rounded-lg shadow-lg shadow-black-100/70"
+    >
+      <View className="flex absolute top-5 right-5 z-50 flex-row items-center p-1 px-2 rounded-full bg-white/90">
+        <Image source={icons.star} className="size-2.5" />
+        <Text className="ml-0.5 text-xs font-rubik-bold text-primary-300">
+          4.4
+        </Text>
+      </View>
+
+      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+
+      <View className="flex flex-col mt-2">
+        <Text className="text-base text-black-300 font-rubik-bold">
+          Cozy Studio
+        </Text>
+        <Text className="text-xs text-black-200 font-rubik">
+          22 W 15th St, New York
+        </Text>
+
+        <View className="flex flex-row justify-between items-center mt-2">
+          <Text className="text-base text-primary-300 font-rubik-bold">
+            $2,500
+          </Text>
+
+          <Image
+            source={icons.heart}
+            className="mr-2 w-5 h-5"
+            tintColor="#191d31"
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
